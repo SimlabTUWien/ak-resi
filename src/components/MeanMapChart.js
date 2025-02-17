58d1c1a4ade558fc1c0c95cc8048cce1ef829036
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-02-17 15:03:32 +0100

    added mobile clicking feature to mm chart

--- src/components/MeanMapChart.js
+++ src/components/MeanMapChart.js
@@ -32,7 +32,7 @@ const tooltipMap = {
 const textinfoMap = {
   "b": {
     title: "Burgenland",
-    text: "Informationen über Burgenland"
+    text: "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua."
   },
   "k": {
     title: "Kärnten",
@@ -68,6 +68,13 @@ const textinfoMap = {
   }
 }
 
+const isTouchDevice = () => {
+  return (
+    "ontouchstart" in window ||
+    (navigator.maxTouchPoints > 0 && !window.matchMedia("(any-hover: hover)").matches)
+  );
+};
+
 const MeanMapChart = () => {
   const svgRefMeanMap = useRef(null);
 
@@ -101,8 +108,9 @@ const MeanMapChart = () => {
         svg.attr("width", "100%").attr("height", "auto");
         svg.style("display", "block").style("margin", "auto");
 
-        // Tooltip setup
-        const tooltip = d3.select("body")
+        if (!isTouchDevice()) {
+          // Tooltip setup
+          const tooltip = d3.select("body")
           .append("div")
           .style("position", "absolute")
           .style("background", "rgba(0, 0, 0, 0.75)")
@@ -111,100 +119,145 @@ const MeanMapChart = () => {
           .style("border-radius", "5px")
           .style("visibility", "hidden")
           .style("font-size", "12px");
-        
+      
 
-        // Mouseover circle behavior
-        svg.selectAll("path")
-          .filter(function () {
-            return d3.select(this).attr("id"); // Only select paths with an id
-          })
-          .style("cursor", "pointer")
-          .on("mouseover", function () {
-            d3.select(this).style("filter", "brightness(0.9)");
-          })
-          .on("mouseout", function () {
-            d3.select(this).style("filter", ""); // Remove filter on mouseout
-          })
-          .on("click", function () {
-            const id = d3.select(this).attr("id");
-            if (textinfoMap[id]) {
-              setSelectedInfo(textinfoMap[id]); // Update selected info
-            }
-          });
+          // Mouseover circle behavior
+          svg.selectAll("path")
+            .filter(function () {
+              return d3.select(this).attr("id"); // Only select paths with an id
+            })
+            .style("cursor", "pointer")
+            .on("mouseover", function () {
+              d3.select(this).style("filter", "brightness(0.9)");
+            })
+            .on("mouseout", function () {
+              d3.select(this).style("filter", ""); // Remove filter on mouseout
+            })
+            .on("click", function () {
+              const id = d3.select(this).attr("id");
+              if (textinfoMap[id]) {
+                setSelectedInfo(textinfoMap[id]); // Update selected info
+              }
+            });
 
-        // Mouseover rect behavior
-        svg.selectAll("rect")
-          .filter(function () {
-            const id = d3.select(this).attr("id");
-            return id !== null && id !== "2500" && id !== "2501" && id !== "10001" && id !== "100001";
-          })
+          // Mouseover rect behavior
+          svg.selectAll("rect")
+            .filter(function () {
+              const id = d3.select(this).attr("id");
+              return id !== null && id !== "2500" && id !== "2501" && id !== "10001" && id !== "100001";
+            })
 
-          .style("cursor", "pointer")
+            .style("cursor", "pointer")
 
-          .on("mouseover", function (event) {
-            const id = d3.select(this).attr("id");
-        
-            // Highlight the hovered rectangle
-            d3.select(this).style("filter", "brightness(0.9)");
-        
-            // Find corresponding legend element by matching legend box ID with population of hovered element
-            const dataKey = `${id}`;
-            if (dataMap[dataKey]) {
-              const populationValue = dataMap[dataKey].population;
-              const residualIncome = dataMap[dataKey].residualIncome;
-        
-              // Find the matching legend element by population
+            .on("mouseover", function (event) {
+              const id = d3.select(this).attr("id");
+          
+              // Highlight the hovered rectangle
+              d3.select(this).style("filter", "brightness(0.9)");
+          
+              // Find corresponding legend element by matching legend box ID with population of hovered element
+              const dataKey = `${id}`;
+              if (dataMap[dataKey]) {
+                const populationValue = dataMap[dataKey].population;
+                const residualIncome = dataMap[dataKey].residualIncome;
+          
+                // Find the matching legend element by population
+                svg.selectAll("rect")
+                  .filter(function () {
+                    const legendId = d3.select(this).attr("id");
+                    return legendId === populationValue.toString(); // Match with population value
+                  })
+                  .style("fill", "#f0d1c7")
+                  .style("stroke", "black")
+                  .style("stroke-width", "2px");
+                  // .style("filter", "brightness(0.92)");
+
+
+                svg.selectAll("text")
+                  .filter(function () {
+                    const textId = d3.select(this).attr("id");
+                    return textId === residualIncome.toString();
+                  })
+                  .style("opacity", "1")
+                  .style("font-weight", "bold");
+
+                // svg.selectAll("text")
+                //   .filter(function () {
+                //     const textId = d3.select(this).attr("id");
+                //     return textId !== null && textId !== residualIncome.toString();
+                //   })
+                //   .style("opacity", "0.75")
+                //   .style("font-weight", "normal");  
+              }
+          
+              // Tooltip display
+              const info = tooltipMap[dataKey] || ["Keine Daten verfügbar", "Keine Daten verfügbar"];
+              tooltip.html(`<strong>${info[0]}</strong><br/>${info[1]}`)
+                .style("visibility", "visible")
+                .style("top", `${event.pageY - 30}px`)
+                .style("left", `${event.pageX + 15}px`);
+            })
+            .on("mousemove", function (event) {
+              tooltip.style("top", `${event.pageY - 30}px`)
+                .style("left", `${event.pageX + 15}px`);
+            })
+            .on("mouseout", function (event) {
+              d3.select(this).style("filter", "");
+
               svg.selectAll("rect")
-                .filter(function () {
-                  const legendId = d3.select(this).attr("id");
-                  return legendId === populationValue.toString(); // Match with population value
-                })
-                .style("filter", "brightness(0.92)")
-                .style("stroke", "black")
-                .style("stroke-width", "2px");
-
+                .style("stroke", "")
+                .style("stroke-width", "")
+                .style("fill", "");
+                // .style("filter", "");
 
               svg.selectAll("text")
-                .filter(function () {
-                  const textId = d3.select(this).attr("id");
-                  return textId === residualIncome.toString();
-                })
-                .style("font-weight", "bold");
+                .style("font-weight", "normal")
+              // svg.selectAll("text")
+              //   .style("font-weight", "normal")
+              //   .style("opacity", "1");  
+              // if (!event.relatedTarget || event.relatedTarget.tagName !== "rect") {
+              //   svg.selectAll("text")
+              //     .style("font-weight", "normal")
+              //     .style("opacity", "1");
+              // }
 
-              svg.selectAll("text")
-                .filter(function () {
-                  const textId = d3.select(this).attr("id");
-                  return textId !== null && textId !== residualIncome.toString();
-                })
-                .style("opacity", "0.75")
-                .style("font-weight", "normal");  
-            }
-        
-            // Tooltip display
-            const info = tooltipMap[dataKey] || ["Keine Daten verfügbar", "Keine Daten verfügbar"];
-            tooltip.html(`<strong>${info[0]}</strong><br/>${info[1]}`)
-              .style("visibility", "visible")
-              .style("top", `${event.pageY - 30}px`)
-              .style("left", `${event.pageX + 15}px`);
-          })
-          .on("mousemove", function (event) {
-            tooltip.style("top", `${event.pageY - 30}px`)
-              .style("left", `${event.pageX + 15}px`);
-          })
-          .on("mouseout", function () {
-            d3.select(this).style("filter", "");
+              tooltip.style("visibility", "hidden");
+            });
+        }
+        else {
+          svg.selectAll("path")
+            .filter(function () {
+              return d3.select(this).attr("id"); // Only select paths with an id
+            })
+            .style("cursor", "pointer")
+            .on("mouseover", function () {
+              d3.select(this).style("filter", "brightness(0.9)");
+            })
+            .on("mouseout", function () {
+              d3.select(this).style("filter", ""); // Remove filter on mouseout
+            })
+            .on("click", function () {
+              const id = d3.select(this).attr("id");
+              if (textinfoMap[id]) {
+                setSelectedInfo(textinfoMap[id]); // Update selected info
+              }
+            });
 
-            svg.selectAll("rect")
-              .style("stroke", "")
-              .style("stroke-width", "")
-              .style("filter", "");
+          svg.selectAll("g")
+            .on("click", function () {
+              const classList = d3.select(this).attr("class");
+              if (classList) {
+                const classes = classList.split(" "); // Convert to array
+                const matchedClass = classes.find(cls => textinfoMap[cls]); // Find the first matching class
+            
+                if (matchedClass) {
+                  setSelectedInfo(textinfoMap[matchedClass]); // Update selected info
+                }
+              }
+            });
+        }
 
-            svg.selectAll("text")
-              .style("font-weight", "normal")
-              .style("opacity", "1");  
-
-            tooltip.style("visibility", "hidden");
-          });
+    
       } catch (error) {
         console.error("Error loading SVG:", error);
       }
@@ -219,10 +272,11 @@ const MeanMapChart = () => {
     <Box 
         className="infotext-container"
         sx={{
-          boxShadow: 2, // Use shadow from MUI's theme (0-24)
+          // boxShadow: 2,
+          // backgroundColor: "white",
           padding: 2,
           borderRadius: 2,
-          backgroundColor: "white"
+          minHeight: '140px'
         }}
       >
         <div className="info-navigation">
