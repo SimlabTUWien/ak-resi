9d71fdd8ada7971a6affd1d6b30cbe16c032f3de
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-04-23 15:04:47 +0200

    feat: add english version of imprint page

--- src/pages/Impressum.js
+++ src/pages/Impressum.js
@@ -1,34 +1,121 @@
 import React from "react";
+import { useLanguage } from "../context/LanguageContext";
 
 export default function Impressum() {
+
+   const { language } = useLanguage();
+
+   const translations = {
+    DE: {
+      header: 'Impressum',
+      content: [
+        {
+          id: 1,
+          text: (
+          <>
+            Tatjana Neuhuber
+          </>
+          ),
+          className: 'boldText'
+        },
+        {
+          id: 2,
+          text: (
+          <>
+            Forschungsbereich Finanzwissenschaft und Infrastrukturpolitik
+          </>
+          ),
+        },
+        {
+          id: 3,
+          text: (
+          <>
+            Institut für Raumplanung
+          </>
+          ),
+        },
+        {
+          id: 4,
+          text: (
+          <>
+            TU Wien
+          </>
+          ),
+        },
+        {
+          id: 5,
+          text: (
+          <>
+            Karlsgasse 11, 1040 Wien, Österreich
+          </>
+          ),
+        },
+      ]
+    },
+    EN: {
+      header: 'Imprint',
+      content: [
+        {
+          id: 1,
+          text: (
+            <>
+              Tatjana Neuhuber
+            </>
+          ),
+          className: 'boldText'
+        },
+        {
+          id: 2,
+          text: (
+            <>
+              Research Unit for Public Finance and Infrastructure Policy
+            </>
+          ),
+        },
+        {
+          id: 3,
+          text: (
+            <>
+              Institute of Spatial Planning
+            </>
+          ),
+        },
+        {
+          id: 4,
+          text: (
+            <>
+              TU Wien
+            </>
+          ),
+        },
+        {
+          id: 5,
+          text: (
+            <>
+              Karlsgasse 11, 1040 Vienna, Austria
+            </>
+          ),
+        },
+      ]
+    }
+  };
+
+  const t = translations[language] || translations.DE;
+
   return (
     <>
       <div className='title-wrapper'>
           <section className="title-section">
-            <h1 className="title">Impressum</h1>
+            <h1 className="title">{t.header}</h1>
           </section>
       </div>
 
       <div className="content-wrapper">
-        <p className="paragraph-impressum boldText">
-          Tatjana Neuhuber
-        </p>
-
-        <p className="paragraph-impressum">
-          Forschungsbereich Finanzwissenschaft und Infrastrukturpolitik
-        </p>
-
-        <p className="paragraph-impressum">
-          Institut für Raumplanung
-        </p>
-
-        <p className="paragraph-impressum">
-          TU Wien
-        </p>
-
-        <p className="paragraph-impressum">
-          Karlsgasse 11, 1040 Wien, Österreich
-        </p>
+        {t.content.map((item) =>
+            <p key={item.id} className={`paragraph-impressum ${item.className || ''}`}>
+                {item.text}
+            </p>
+        )}
 
       </div>
     </>
