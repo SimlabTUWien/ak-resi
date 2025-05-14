c3e2d95d07c94b527212c85469382997c59db9b2
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-05-14 16:38:41 +0200

    feat: add dynamic coloring for floating button

--- src/components/FloatingButton.js
+++ src/components/FloatingButton.js
@@ -1,9 +1,19 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import { motion } from "framer-motion";
 
+const sectionColors = {
+  intro: '#bdbdbd',
+  income: '#bdbdbd',
+  residualIncome: '#f0ae9f',
+  'social-infrastructure': '#a5cdc8',
+  'time-usage': '#fcd799',
+  'what-now': '#bdbdbd',
+};
+
 const FloatingButton = ({ show }) => {
   const [isVisible, setIsVisible] = useState(window.innerWidth > 1300);
+  const [buttonColor, setButtonColor] = useState('#b3b3b3');
 
   useEffect(() => {
     const handleResize = () => {
@@ -14,6 +24,25 @@ const FloatingButton = ({ show }) => {
     return () => {
       window.removeEventListener("resize", handleResize);
     };
+  }, []);
+
+   useEffect(() => {
+    const handleScroll = () => {
+      let currentColor = '#bdbdbd';
+      Object.keys(sectionColors).forEach((id) => {
+        const section = document.getElementById(id);
+        if (section) {
+          const rect = section.getBoundingClientRect();
+          if (rect.top <= 50 && rect.bottom >= 50) {
+            currentColor = sectionColors[id];
+          }
+        }
+      });
+      setButtonColor(currentColor);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   const scrollToIntro = () => {
@@ -28,11 +57,18 @@ const FloatingButton = ({ show }) => {
   if (!isVisible) return null;
 
   return (
-    <motion.button className="floating-button" onClick={scrollToIntro}
+    <motion.button 
+      className="floating-button" 
+      onClick={scrollToIntro}
       initial={{ opacity: 0, y: 20 }}
       animate={{ opacity: show ? 1 : 0, y: show ? 0 : 20 }}
-      transition={{ duration: 1.5, ease: "easeInOut" }} // Smooth transition
+      transition={{ duration: 1.5, ease: "easeInOut" }}
       disabled={!show}
+      style={{
+        backgroundColor: 'white',
+        borderColor: buttonColor,
+        color: buttonColor,
+      }}
     >
       <ArrowUpwardIcon/>
     </motion.button>
