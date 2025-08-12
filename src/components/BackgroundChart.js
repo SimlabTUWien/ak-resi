2f2c10c295985e68375a38bafe616122e0bbaaa5
Rasakul13 <39884629+Rasakul13@users.noreply.github.com>
2025-08-12 16:31:05 +0200

    fix: removed language context; removed unused svg files

--- src/components/BackgroundChart.js
+++ src/components/BackgroundChart.js
@@ -23,7 +23,7 @@ const textMap = {
         },
         "soziale_infrastrukturen": {
           title: "Soziale Infrastrukturen",
-          text: "Soziale Infrastrukturen sind für das Funktionieren einer Gesellschaft genauso essenziell wie technische Infrastrukturen, also Straßen oder Telefonverbindungen. Zugang zu Kindergärten, Schulen, Ärzt:innen, Krankenhäusern, Pflegeheimen und ähnlichen sozialen Angeboten sind für große Teile der Bevölkerung unverzichtbar und dennoch sind diese Einrichtungen nicht überall in gleicher Qualität oder Dichte verfügbar, was regionale Unterschiede in der Lebensqualität verstärken kann.",
+          text: "Soziale Infrastrukturen sind für das Funktionieren einer Gesellschaft genauso essenziell wie technische Infrastrukturen, also Straßen oder Telefonverbindungen. Zugang zu Kindergärten, Schulen, Ärzt:innen, Krankenhäusern, Pflegeeinrichtungen und ähnlichen sozialen Angeboten sind für große Teile der Bevölkerung unverzichtbar und dennoch sind diese Einrichtungen nicht überall in gleicher Qualität oder Dichte verfügbar, was regionale Unterschiede in der Lebensqualität verstärken kann.",
           color: "#a5cdc8"
         },
         "zeit_verwendung": {
@@ -55,48 +55,7 @@ const textMap = {
     alt: "Diagramm mit vier überlappenden Kreisen, welche die Themenbereiche dieser Webseite visualisieren"
   },
   EN: {
-    elements: {
-      "einkommen": {
-        title: "Income",
-        text: "Household income comes from various sources, such as employment, self-employment, pensions, or social benefits. In Austria, the median household income is €2,632, which means that 50% of households have more and 50% have less than this amount available per month.",
-        color: "#BDBDBD"
-      },
-      "notwendige_ausgaben": {
-        title: "Necessary Expenses",
-        text: "Expenses for covering basic needs are costs that are unavoidable or difficult to reduce. These include housing, heating, energy, food, transportation, and costs for education and health. These necessary expenses can vary greatly depending on the region, type of housing, or access to public infrastructure.",
-        color: "#f0ae9f"
-      },
-      "soziale_infrastrukturen": {
-        title: "Social Infrastructures",
-        text: "Social infrastructures are just as essential for the functioning of a society as technical infrastructures, such as roads or phone networks. Access to kindergartens, schools, doctors, hospitals, nursing homes, and similar social services is indispensable for large parts of the population. However, these services are not always available in the same quality or density everywhere, which can reinforce regional differences in quality of life.",
-        color: "#a5cdc8"
-      },
-      "zeit_verwendung": {
-        title: "Time Use",
-        text: "Everyone has 24 hours a day, but how these hours are used varies greatly. The time spent each day on paid work, unpaid activities like household chores or childcare, leisure, commuting, and sleep is different for everyone. These differences influence both the quality of life and the possibilities to earn income or participate in social activities.",
-        color: "#fcd799"
-      },
-      "residualeinkommen": {
-        title: "Residual Income",
-        text: "Residual income describes the money left after deducting necessary expenses for housing, energy, health, etc. This remaining money can be used for irregular expenses, leisure activities, or savings and plays an important role in a household’s financial freedom.",
-        color: "#f0d1c7"
-      },
-      "notwendige_ausgaben_und_SI": {
-        title: "Necessary Expenses and SI",
-        text: "Publicly accessible and affordable educational and health services reduce households’ necessary expenses, thus easing the financial burden. However, if these services are difficult to access, mobility costs rise, or households may have to rely on expensive private options like doctors without insurance coverage.",
-        color: "#d2d5cb"
-      },
-      "zeit_und_infrastruktur": {
-        title: "Time and Infrastructure",
-        text: "Social infrastructures have a big impact on how people can spend their time. A conveniently located kindergarten allows parents to spend more time in paid work. Long distances to schools or healthcare facilities, on the other hand, lead to longer commutes or – in emergencies – increased risks when help arrives too late.",
-        color: "#e9dfbe"
-      },
-      "zeit_als_ressource": {
-        title: "Time as a Resource",
-        text: "Those who invest more time in paid work can potentially earn higher incomes but have less time for other (unpaid) tasks like housework, childcare, or volunteering. People with higher incomes can use their time differently – for example, by paying for services like household help or childcare to free up time for other activities.",
-        color: "rgb(247, 227, 196)"
-      }
-    },
+    elements: {},
     alt: "Diagram with four overlapping circles visualizing the content areas of this website"
   }
  
@@ -116,10 +75,7 @@ const BackgroundChart = () => {
   const localizedDataMap = textMap[language];
 
   const svgRef = useRef(null);
-  // const svgUrl = `${process.env.PUBLIC_URL}/images/1_4Kreise4Overlaps_${language.toUpperCase()}.svg`;
-  const svgUrl = `${process.env.PUBLIC_URL}/images/1_4Kreise4Overlaps_${language.toUpperCase()}_V2.svg`;
-
-
+  const svgUrl = `${process.env.PUBLIC_URL}/images/1_4Kreise4Overlaps.svg`;
 
 
   // const [isVisible, setIsVisible] = useState(isTouchDevice());
